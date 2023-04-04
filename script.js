1369d6153de16ec333d02b2e8453f2585aec44cc
tgorsk15 <tgbusiness463@gmail.com>
2023-04-04 18:10:07 -0500

    Create containers for button and grid while adding button logic
    Created two separate divs attached to the body in html that will serve to separate the grid
    generating button, and the actual grid. Gave the button an addEvent Listener that will prompt
    the user with how big they want grid to be

---
+++ script.js
@@ -0,0 +1,10 @@
+// grid generator button
+
+const generateButton = document.querySelector('.grid-button')
+generateButton.addEventListener('click', () => {
+    answer = prompt('How many sqaures tall and wide do you want your grid?');
+
+    
+})
+
+// grid
