3ec10f4da072980b3135b214e40fe13c445d81ea
tgorsk15 <tgbusiness463@gmail.com>
2023-04-05 13:15:20 -0500

    Create skeletons for grid functions
    Have created createGrid() and createBox() to try to divide the work of creating the ititial box,
    and then multiplying that box into a grid. I am currently trying to get a for loop to work
    so that the correct amount of boxes are created and added to the grid container

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// grid generator button
+// grid reference
+gridContainer = document.querySelector('.grid-contianer')
+// const box = document.createElement('box')
 
+
+// grid
+// IDEA: use a loop to generate a single box in the grid, and set the stop condition in the
+// loop to whatever the inputted number is from the button
+let answer = 16;
+
+
+function createGrid() {
+    // loop for multiplying amount of boxes
+}
+
+//Box generator function
+function createBox() {
+    for (i = 0; i < answer; i++) {
+        const box = document.createElement('box')
+        gridContainer.appendChild('box')
+    }
+    console.log(answer)
+}
+
+
+
+// grid generator button
 const generateButton = document.querySelector('.grid-button')
 generateButton.addEventListener('click', () => {
     answer = prompt('How many sqaures tall and wide do you want your grid?');
-
-    
+    // createBox();
 })
 
-// grid
+createBox()
+
+// Color change for mouse hover
